bb879a255a3c3eded6d78c2f4c684f766185d844
xplshn <xplshn@murena.io>
2024-09-16 00:01:59 -0300

    Added a so-called 'hebang' line to main.go, yes, you can execute the main.go as if it was a shell script
    Signed-off-by: xplshn <xplshn@murena.io>

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+//usr/bin/env go run findURL.go fsearch.go info.go install.go listBinaries.go main.go remove.go run.go update.go utility.go "$@"; exit $?
+// dbin - 📦 Poor man's package manager. The easy to use, easy to get, suckless software distribution system
 package main
 
 import (
