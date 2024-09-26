c3e44f8f5ac21fc1988019efff7563686738d227
xplshn <xplshn@murena.io>
2024-09-26 20:45:15 -0300

    Modified the program that creates the dbin-metadata

--- misc/cmd/modMetadata/main.go
+++ misc/cmd/modMetadata/main.go
@@ -142,6 +142,10 @@ func main() {
 			{"https://bin.ajam.dev/" + arch + "/Baseutils/METADATA.json",
 				"https://huggingface.co/datasets/Azathothas/Toolpacks-Snapshots/resolve/main/Baseutils/METADATA.json?download=true",
 				"Baseutils"},
+			// --- Toolpacks-extra
+				{"https://huggingface.co/datasets/Azathothas/Toolpacks-Extras/resolve/main/" + arch + "/METADATA.json",
+				"https://huggingface.co/datasets/Azathothas/Toolpacks-Extras/resolve/main/",
+				"Toolpacks-extras"},
 		}
 
 		for _, repo := range repos {
