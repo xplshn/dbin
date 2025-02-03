8b068c065c7c7c2cfbf81d752d2aa07fd5f89596
xplshn <xplshn@murena.io>
2025-02-03 05:19:15 -0300

    Use GZ metadata
    Signed-off-by: xplshn <xplshn@murena.io>

--- config.go
+++ config.go
@@ -248,7 +248,7 @@ func setDefaultValues(config *Config) {
 	// Set up default metadata URLs if none are provided
 	if len(config.MetadataURLs) == 0 {
 		config.MetadataURLs = []string{
-			"https://github.com/xplshn/dbin-metadata/raw/refs/heads/master/misc/cmd/modMetadata/METADATA_" + arch + ".min.json",
+			"https://github.com/xplshn/dbin-metadata/raw/refs/heads/master/misc/cmd/modMetadata/METADATA_" + arch + ".min.json.gz",
 		}
 	}
 
